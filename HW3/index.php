e99c2dc218d3e90d18572e4c50f66e7af911eeba
Bassam1u <154407562+Bassam1u@users.noreply.github.com>
2026-01-22 20:28:13 +0300

    Add files via upload

---
+++ HW3/index.php
@@ -0,0 +1,22 @@
+
+<?php
+  $student =["bassam","majid","ahmad","alsalafi"];
+
+  foreach($student as $stu){
+    echo "   انت تعرف من انت لاتحتاج نصائح  $stu    مرحباً  <br>  ";
+   
+}
+echo "#******#*****#*****#*****#*****#<br>";
+
+
+
+
+
+$color =['black' => '31'];
+for ($num =1; $num<=10; $num++){
+    if ($num % 2 == 0){
+        echo " <br>   Double The Number هس  :👉 $num 👈<br>";
+    }
+    
+}
+?>
